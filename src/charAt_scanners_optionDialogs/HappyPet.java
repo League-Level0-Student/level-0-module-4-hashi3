c60b5ca16035daed2049fe18288230c4e5a944b6
california <calliannakaylin@gmail.com>
2019-05-22 17:56:31 -0700

    did math and happiness level.

--- src/charAt_scanners_optionDialogs/HappyPet.java
+++ src/charAt_scanners_optionDialogs/HappyPet.java
@@ -5,57 +5,65 @@ public class HappyPet {
 	
 	// 2. Add the following variable to the next line: static int happinessLevel = 0;
 	// this will be used to store the happiness of your pet
-	static int happinessLevel = 0;
-	static String[] petlists = {"cuddle", "food", "water", "take a walk", "groom", "clean up poop"};
-	
+	private static int happinessLevel = 0;
 	public static void main(String[] args) {
-		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
-		String pet = JOptionPane.showInputDialog( "what kind pet do you want");
-
-		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
 		
-			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
-			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
-			//    Make sure to customize the title and question too.
-          int selection = JOptionPane.showOptionDialog(null, "selec the option", " pets questions ", JOptionPane.YES_OPTION, JOptionPane.QUESTION_MESSAGE, null, petlists, 0);
-          System.out.println(selection);
-			// 5. Use user input to call the appropriate method created in step 4.
+	
+		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
+		String Pet = JOptionPane.showInputDialog("What kind of pet would you like to buy?");
 
-			// 6. If you determine the happiness level is large enough, tell the
+		//int task = JOptionPane.showOptionDialog(null, "WHAT WOULD YOU LIKE TO DO TO MAKE YOUR PET HAPPIER?", "HAPPY PET HAPPY LIFE", 0, JOptionPane.INFORMATION_MESSAGE, null, new String [] {"Feed it","Walk it","Scratch it"},null);
+
+
+		
+		 while(true) {
+				int task = JOptionPane.showOptionDialog(null, "WHAT WOULD YOU LIKE TO DO TO MAKE YOUR PET HAPPIER?", "HAPPY PET HAPPY LIFE", 0, JOptionPane.INFORMATION_MESSAGE, null, new String [] {"Feed it","Walk it","Scratch it"},null);
+
+			 if( task == 0) {
+				  feed();
+			  }		  
+			  else if(task == 1) {
+				  walk_it();
+			  }		  
+			  else if(task == 2) {
+			      scratch();	  
+			  }
+			  else {
+				  System.out.println("that is not an option");
+			  }	
+			 
+			 System.out.println (happinessLevel);
+			 if(happinessLevel== 100) {
+			   JOptionPane.showMessageDialog(null, "you love your " + Pet );
+			   break;
+			 }
+			 
+		 }
+
+	}	// 6. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
-  // use switch statement
-	}
+
+	
 
 	// 4. Create methods to handle each of your user selections.
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
 	//    and INCREMENT the pet's happiness Level.
-	static void cuddle(){
-		JOptionPane.showMessageDialog(null, "good dog");
-		happinessLevel++;
-	}
-	static void food() {
-		JOptionPane.showMessageDialog(null, "eat up");
-		happinessLevel++;
 
-	}
-	static void water() {
-		JOptionPane.showMessageDialog(null, "drink up");
-		happinessLevel++;
-	}
-	static void  takeAwalk() {
-		JOptionPane.showMessageDialog(null, "let's go");
-		happinessLevel++;
-	}
-	static void room() {
-		JOptionPane.showMessageDialog(null, "groom");
-		happinessLevel++;
-	}
-	static void cleanUppoop() {
-		JOptionPane.showMessageDialog(null, "drink up" );	
-		happinessLevel++;
 
-	}
+public static void feed() {
+//	JOptionPane
 	
+	happinessLevel += 10;
+}
+public static void walk_it() {
+//	JOptionPane
 	
+	happinessLevel += 10;
+}
+public static void scratch () {
+//	JOptionPane
 	
+	happinessLevel += 10;
+}
+
 }
--- src/doubles_and_booleans/ChangeCalculator.java
+++ src/doubles_and_booleans/ChangeCalculator.java
@@ -15,15 +15,26 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
+		
+	String Money1 =	JOptionPane.showInputDialog("how many nickels do you have");
 
 		// Convert their answer to an int using Integer.parseInt()
+	int nickel = Integer.parseInt(Money1);
 
 		// Ask the user how many dimes they have, and convert their answer
+	
+	String Money2 =	JOptionPane.showInputDialog("how many dimes do you have");
+	int dimes = Integer.parseInt(Money2);
 
-		// Ask the user how many quarters they have, and convert their answer
+
+	// Ask the user how many quarters they have, and convert their answer
+	String Money3 =	JOptionPane.showInputDialog("how many qaurter do you have");
+	int quarter = Integer.parseInt(Money3);
 
 		// Calculate how much money the user has and save it in a double variable 
+int total = ((nickel * 5) + (dimes * 10)  + (quarter * 25)) / 100;
 
+JOptionPane.showMessageDialog(null, "you have " + total + " dollors");
 		// Tell the user how much money they have
 
 	}
